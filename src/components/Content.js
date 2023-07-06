2374c76077514269bdbb1ceb44158d19ba7829e6
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-07 00:33:47 +0330

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Region from "./ContentComponents/Region";
+import Plan from "./ContentComponents/Plan";
+import OperatingSystem from "./ContentComponents/OperatingSystem";
+import SSHkey from "./ContentComponents/SSHkey";
+import HostName from "./ContentComponents/HostName";
+import InstanceDeploy from "./ContentComponents/InstanceDeploy";
 
 const Content = () => {
   return (
     <>
+      <Region/>
+      <Plan/>
+      <OperatingSystem/>
+      <SSHkey/>
+      <HostName/>
+      <InstanceDeploy/>
     </>
   );
 }

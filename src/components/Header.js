e83e24632228617aca7c9a79fb8a00a4ec05db23
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-07 16:42:45 +0330

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Box, Typography, Breadcrumbs, Link } from "@mui/material";
+import { Box, Typography, Breadcrumbs, Badge, Avatar } from "@mui/material";
+import { NotificationsNoneOutlined, AddOutlined } from "@mui/icons-material";
+import { SideBarWidth } from "../Config";
+
+const balance = 125.5;
 
 const Header = () => {
   return (
     <Box
       display={"flex"}
-      justifyContent={"center"}
+      justifyContent={"space-between"}
       alignItems={"center"}
       height={"60px"}
+      width={`calc(100vw - ${SideBarWidth}px)`}
       // paddingLeft={"10px"}
     >
       <Breadcrumbs aria-label="breadcrumb">
@@ -18,6 +23,16 @@ const Header = () => {
           New Instance
         </Typography>
       </Breadcrumbs>
+
+      <Badge color="error" variant="dot">
+        <NotificationsNoneOutlined />
+      </Badge>
+
+      <Box sx={{ border: "1px solid gray", borderRadius: "5px" }}>
+        <AddOutlined />${balance}
+      </Box>
+
+      <Avatar />
     </Box>
   );
 };

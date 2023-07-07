a8a75f5d3f737bbb28189394c1ebe9d3d5a4af4e
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-07 15:57:40 +0330

    delete body margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
-import { Box } from "@mui/material";
 import React from "react";
+import { Box, Typography, Breadcrumbs, Link } from "@mui/material";
 
 const Header = () => {
-  return <Box>Header</Box>;
+  return (
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      height={"60px"}
+      // paddingLeft={"10px"}
+    >
+      <Breadcrumbs aria-label="breadcrumb">
+        <Typography color="inherit" fontSize={18}>
+          Instances
+        </Typography>
+        <Typography color="text.primary" fontSize={18}>
+          New Instance
+        </Typography>
+      </Breadcrumbs>
+    </Box>
+  );
 };
 
 export default Header;

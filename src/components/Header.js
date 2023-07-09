b1a046ea5c6abe8f7d7a5abb827957f5de5aab5d
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-09 14:38:25 +0330

    complete header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Typography, Breadcrumbs, Badge, Avatar } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Breadcrumbs,
+  Badge,
+  Avatar,
+  Divider,
+  Grid,
+} from "@mui/material";
 import { NotificationsNoneOutlined, AddOutlined } from "@mui/icons-material";
 import { SideBarWidth } from "../Config";
 
@@ -7,13 +15,14 @@ const balance = 125.5;
 
 const Header = () => {
   return (
-    <Box
+    <Grid
       display={"flex"}
+      flexDirection={"row"}
       justifyContent={"space-between"}
       alignItems={"center"}
       height={"60px"}
       width={`calc(100vw - ${SideBarWidth}px)`}
-      // paddingLeft={"10px"}
+      paddingX={"30px"}
     >
       <Breadcrumbs aria-label="breadcrumb">
         <Typography color="inherit" fontSize={18}>
@@ -24,16 +33,43 @@ const Header = () => {
         </Typography>
       </Breadcrumbs>
 
-      <Badge color="error" variant="dot">
-        <NotificationsNoneOutlined />
-      </Badge>
-
-      <Box sx={{ border: "1px solid gray", borderRadius: "5px" }}>
-        <AddOutlined />${balance}
-      </Box>
-
-      <Avatar />
-    </Box>
+      <Grid
+        container
+        direction={"row"}
+        display={"flex"}
+        justifyContent={"space-around"}
+        alignItems={"center"}
+        maxWidth={"300px"}
+      >
+        <Badge
+          color="error"
+          variant="dot"
+          sx={{ position: "relative", top: "5px" }}
+        >
+          <NotificationsNoneOutlined
+            sx={{ position: "relative", top: "-5px", left: "5px" }}
+          />
+        </Badge>
+        <Grid
+          display={"flex"}
+          justifyContent={"flex-start"}
+          alignItems={"center"}
+          sx={{
+            border: "1px solid rgb(189,189,189)",
+            borderRadius: "5px",
+            minWidth: "150px",
+            py: "5px",
+          }}
+        >
+          <AddOutlined color="action" sx={{ px: "5px", cursor: "pointer" }} />
+          <Typography color={"rgba(0,0,0,0.8)"}>
+            $ {balance.toFixed(2)}
+          </Typography>
+        </Grid>
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <Avatar />
+      </Grid>
+    </Grid>
   );
 };
 

cd519b8e93c641d969c7b0e8d678951854115755
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-09 18:01:19 +0330

    Update Region.js

--- src/components/ContentComponents/Region.js
+++ src/components/ContentComponents/Region.js
@@ -4,6 +4,7 @@ import { BASE_API_ROUTE } from "../../Config";
 
 const Region = () => {
   const [regions, setRegions] = useState([]);
+  const [countryFlags, setCountryFlags] = useState([]);
 
   useEffect(() => {
     const getRegions = async () => {
@@ -12,12 +13,35 @@ const Region = () => {
         const response = await axios.get(url);
         // console.log("response in getting regions : ", response);
         setRegions(response.data);
-        console.log(response.data);
       } catch (error) {
         console.log("error in getting regions : ", error);
       }
     };
+
+    const getCountryFlags = async () => {
+      try {
+        const countryCodes = await axios.get(
+          "https://flagcdn.com/en/codes.json"
+        );
+        regions.forEach((region) => {
+          const regionName =
+            region.name === "US" ? "United States" : region.name;
+          for (const key in countryCodes.data) {
+            if (regionName === countryCodes.data[key]) {
+              setCountryFlags((prevCountryFlags) => [
+                ...prevCountryFlags,
+                `https://flagcdn.com/w80/${key}.png`,
+              ]);
+            }
+          }
+        });
+      } catch (error) {
+        console.log("error in getting Country Flags : ", error, ";");
+      }
+    };
+
     getRegions();
+    if (countryFlags.length == 0) getCountryFlags();
   }, []);
 
   return <></>;

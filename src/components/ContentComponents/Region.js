a4b4a369231a01948e657c5d85ebc4a68db09b7c
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-09 19:55:05 +0330

    update Region.js
    react-usestateref package added

--- src/components/ContentComponents/Region.js
+++ src/components/ContentComponents/Region.js
@@ -1,50 +1,53 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_API_ROUTE } from "../../Config";
+import { Grid, Typography } from "@mui/material";
 
 const Region = () => {
   const [regions, setRegions] = useState([]);
-  const [countryFlags, setCountryFlags] = useState([]);
 
   useEffect(() => {
     const getRegions = async () => {
-      const url = BASE_API_ROUTE + "/regions";
+      if (regions.length > 0) return;
       try {
-        const response = await axios.get(url);
-        // console.log("response in getting regions : ", response);
-        setRegions(response.data);
-      } catch (error) {
-        console.log("error in getting regions : ", error);
-      }
-    };
-
-    const getCountryFlags = async () => {
-      try {
+        const response = await axios.get(BASE_API_ROUTE + "/regions");
         const countryCodes = await axios.get(
           "https://flagcdn.com/en/codes.json"
         );
-        regions.forEach((region) => {
+        // console.log("response in getting regions : ", response);
+        response.data.forEach((region) => {
           const regionName =
             region.name === "US" ? "United States" : region.name;
           for (const key in countryCodes.data) {
             if (regionName === countryCodes.data[key]) {
-              setCountryFlags((prevCountryFlags) => [
-                ...prevCountryFlags,
-                `https://flagcdn.com/w80/${key}.png`,
+              setRegions((prevRegions) => [
+                ...prevRegions,
+                {
+                  ...region,
+                  flag: `https://flagcdn.com/w80/${key}.png`,
+                },
               ]);
             }
           }
         });
       } catch (error) {
-        console.log("error in getting Country Flags : ", error, ";");
+        console.log("error in getting regions : ", error);
       }
     };
-
     getRegions();
-    if (countryFlags.length == 0) getCountryFlags();
   }, []);
 
-  return <></>;
+  return (
+    <Grid>
+      <Typography>Region</Typography>
+      {regions.map((region, index) => (
+        <Grid key={index}>
+          <img src={region.flag} width="80" alt="country flag" />
+          <Typography>{region.name}</Typography>
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default Region;

866543b24e190d55d336bc87ce34ac3d5836689c
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-09 16:57:42 +0330

    update Regions.js and added axios package

--- src/components/ContentComponents/Region.js
+++ src/components/ContentComponents/Region.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_API_ROUTE } from "../../Config";
 
 const Region = () => {
-  return (
-    <>
-    </>
-  );
-}
+  const [regions, setRegions] = useState([]);
+
+  useEffect(() => {
+    const getRegions = async () => {
+      const url = BASE_API_ROUTE + "/regions";
+      try {
+        const response = await axios.get(url);
+        // console.log("response in getting regions : ", response);
+        setRegions(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log("error in getting regions : ", error);
+      }
+    };
+    getRegions();
+  }, []);
+
+  return <></>;
+};
 
 export default Region;

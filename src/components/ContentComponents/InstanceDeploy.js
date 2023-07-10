4bf1193daca138f34f6345538e9b6174ef31cbc5
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-10 22:48:04 +0330

    Update InstanceDeploy.js

--- src/components/ContentComponents/InstanceDeploy.js
+++ src/components/ContentComponents/InstanceDeploy.js
@@ -1,10 +1,24 @@
+import { Button, Grid } from "@mui/material";
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const InstanceDeploy = () => {
+  const plans = useSelector((state) => state.plans);
+  const selectedPlan = useSelector((state) => state.selectedPlan);
+  const regions = useSelector((state) => state.regions);
+  const selectedRegion = useSelector((state) => state.selectedRegion);
+
+  const handleDeploy = () => {
+    console.log(regions[selectedRegion].name, plans[selectedPlan].cpu_cores);
+  };
+
   return (
-    <>
-    </>
+    <Grid>
+      <Button variant="contained" onClick={handleDeploy}>
+        Deploy
+      </Button>
+    </Grid>
   );
-}
+};
 
 export default InstanceDeploy;
